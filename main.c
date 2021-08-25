b6a758e23e13576189196c3c1c4fc3159ba8a5cd
M Nassar <mnassar@users.noreply.github.com>
2021-08-25 15:08:21 +0000

    added main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<ctype.h>
+int main(void) {
+  char x = 'a'; 
+  printf("Hello World\n");
+  printf("the value of x is %c\n", toupper(x));
+  printf("%s\n", isalpha(x)?"True":"False");
+  return 0;
+}
